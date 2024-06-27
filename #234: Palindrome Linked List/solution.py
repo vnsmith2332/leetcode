745245d54154bd7b788fa884ce55db6a44e0e43d
Victor Smith <vnsmith2332@gmail.com>
2024-06-27 13:15:11 -0500

    Create solution.py

---
+++ #234: Palindrome Linked List/solution.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        curr = head
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            if curr.val != stack.pop():
+                return False
+            curr = curr.next
+        return True
+        
